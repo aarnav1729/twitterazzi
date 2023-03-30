7b7fcbc4ac1dc04167f528b35346a7062ee2e31f
aarnav singh <aarnavsingh836@gmail.com>
2023-03-29 23:57:25 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Twitter API credentials
 consumer_key = 'qZCFDpT52anSAefJWAb4YhLI2'
-consumer_secret = 'YOUR_CONSUMER_SECRET'
-access_token = 'YOUR_ACCESS_TOKEN'
-access_secret = 'csjcJjtyJBKNJwTR2FLxZkQeJnLHJGDHYg0nRSctALTXRkzH6T'
+consumer_secret = 'csjcJjtyJBKNJwTR2FLxZkQeJnLHJGDHYg0nRSctALTXRkzH6T'
+access_token = 'bmtSRXlfcVZqMl9xMEJoYjR1dU06MTpjaQ'
+access_secret = 'b1bOVnC8vE1GCAwjnAy_rxWLOwO4tWVHrXAP3A3sMaBMtCZvt5'
