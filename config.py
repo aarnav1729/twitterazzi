9453541210471a858541a3f999b9a23e5050865e
aarnav singh <aarnavsingh836@gmail.com>
2023-03-29 22:54:29 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API credentials
+consumer_key = 'qZCFDpT52anSAefJWAb4YhLI2'
+consumer_secret = 'YOUR_CONSUMER_SECRET'
+access_token = 'YOUR_ACCESS_TOKEN'
+access_secret = 'csjcJjtyJBKNJwTR2FLxZkQeJnLHJGDHYg0nRSctALTXRkzH6T'
